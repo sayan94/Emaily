5c1f19c729e434366830bf6250ccdf9857e7b733
Sayan Pramanick <sayan94@Sayans-MacBook-Air.local>
2019-03-28 23:58:57 +0530

    Connection to mongodb created

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
+
+
 const app = express();
+
 require('./routes/authroutes')(app);
 
 const PORT = process.env.PORT || 5000
