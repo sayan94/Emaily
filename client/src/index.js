98520d24a85ac8ac242b931cf832914a1f217ccc
Sayan Pramanick <sayanpramanick94@gmail.com>
2019-04-26 15:54:52 +0530

    Bug fix

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App';
 import reducers from './reducers';
